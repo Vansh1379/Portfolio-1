e595a681eb8beb7121b2517022043ab799712503
vansh kalra <vanshkalra1379@gmail.com>
2024-02-17 22:31:55 +0530

    Third commit

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+    function circleMousseFollower(){
+        window.addEventListener("mousemove", function(dets){
+            this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`; 
+        })
+    }
+
+    circleMousseFollower();
